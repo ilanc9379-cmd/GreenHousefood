bec4b0308511f10a3941778f9bec8cf2749e2b40
ilanc9379-cmd <ilanc9379@gmail.com>
2025-08-22 23:34:02 +0200

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="p-4 text-center">
-      <h1 className="text-2xl font-bold">GreenHousefood 🌱</h1>
-      <p>Site en cours de déploiement...</p>
-    </div>
-  )
-}
