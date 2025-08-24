3a5bd032d650d86ba80ee807cabe270f7267e50c
ilanc9379-cmd <ilanc9379@gmail.com>
2025-08-24 02:49:37 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // AUCUN output: 'export' ici
-  experimental: { appDir: false }, // force le Pages Router
-  images: { unoptimized: true },   // pour éviter toute friction côté images
-};
-module.exports = nextConfig;
